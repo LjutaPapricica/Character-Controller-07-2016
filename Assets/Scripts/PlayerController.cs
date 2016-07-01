68bb0341fceafbca0298b372da048fe41d17ffac
Piotr Doan <doanpiotr@gmail.com>
2016-07-02 00:35:23 +0200

    Create an unit prefab with a controller.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject m_player;
+    private UnitController m_controller;
+
+    void Start()
+    {
+        m_controller = m_player.GetComponent<UnitController>();
+    }
+
+    void Update()
+    {
+        Vector3 direction = new Vector3();
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+        direction.Normalize();
+
+        m_controller.MoveDirection(direction);
+    }
+}
---
+++ Assets/Scripts/UnitController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitController : MonoBehaviour
+{
+    public float m_speed;
+
+    private Rigidbody m_rigidbody;
+    private Vector3 m_desiredVelocity;
+
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void MoveDirection(Vector3 direction)
+    {
+        m_desiredVelocity = direction * m_speed;
+    }
+
+    void FixedUpdate()
+    {
+        if(m_desiredVelocity != Vector3.zero)
+        {
+            Vector3 velocityChange = m_desiredVelocity - m_rigidbody.velocity;
+            velocityChange = Vector3.ClampMagnitude(velocityChange, 12.0f * Time.fixedDeltaTime);
+            m_rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
+        }
+    }
+
+    void Update()
+    {
+        Debug.DrawRay(transform.position, m_rigidbody.velocity, Color.red);
+        Debug.DrawRay(transform.position, transform.forward, Color.green);
+    }
+}
