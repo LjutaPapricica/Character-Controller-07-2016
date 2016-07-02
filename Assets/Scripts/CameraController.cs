94ec6fd5b1d468b443c1b89a1edaa8f6bd517983
Piotr Doan <doanpiotr@gmail.com>
2016-07-02 05:33:53 +0200

    Implement a third person camera.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public Camera m_camera;
+    public PlayerController m_playerController;
+    public Vector3 m_offset;
+    private float m_angle;
+
+	void Start()
+    {
+	}
+
+	void Update()
+    {
+        Transform cameraTransform = m_camera.transform;
+        Transform playerTransform = m_playerController.m_unit.transform;
+        
+        // Update camera rotation.
+        cameraTransform.rotation *= Quaternion.Euler(Vector3.up * Input.GetAxis("Mouse X"));
+
+        // Update camera position.
+        cameraTransform.position = playerTransform.position + cameraTransform.rotation * m_offset;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,21 +3,25 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject m_player;
-    private UnitController m_controller;
+    public GameObject m_unit;
+    public CameraController m_cameraController;
+    private UnitController m_unitController;
 
     void Start()
     {
-        m_controller = m_player.GetComponent<UnitController>();
+        m_unitController = m_unit.GetComponent<UnitController>();
     }
 
     void Update()
     {
+        // Calculate direction from user input.
         Vector3 direction = new Vector3();
         direction.x = Input.GetAxis("Horizontal");
         direction.z = Input.GetAxis("Vertical");
         direction.Normalize();
 
-        m_controller.MoveDirection(direction);
+        // Move unit in a direction relative to the camera.
+        Transform cameraTransform = m_cameraController.m_camera.transform;
+        m_unitController.MoveDirection(cameraTransform.rotation * direction);
     }
 }
